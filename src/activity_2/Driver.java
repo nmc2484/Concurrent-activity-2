2b7ed1271328f8a69cd058ecf477cb0af6998612
Matt Alioto <matthew.j.alioto@gmail.com>
2012-03-25 17:23:04 -0400

    Commented all methods and classes.

--- src/activity_2/Driver.java
+++ src/activity_2/Driver.java
@@ -2,15 +2,40 @@ package activity_2;
 
 import java.util.ArrayList;
 
+/**
+ * Driver class for the Dining Philosopher's problem 
+ * 
+ * An arbitrary number of philosophers compete for an identical
+ * number of forks. Each philosopher cycles between thinking and eating
+ * a designated number of times.
+ */
 public class Driver {
 
+	/**
+	 * Program entry point. Creates an array of forks and assigns
+	 * them to philosophers as appropriate. 
+	 * 
+	 * @param args: [-l] [np] [nt] [tm] [em]
+	 * [-l] odd-numbered philosophers are left handed
+	 * 		default: all philosophers are right handed
+	 * [np] number of philosophers and forks
+	 * 		default: 4
+	 * [nt] number of think/eat cycles
+	 * 		default: 10
+	 * [tm] think time in milliseconds
+	 * 		default: 0
+	 * [em] eat time in milliseconds
+	 * 		default: 0
+	 */
 	public static void main(String[] args){
-		//TODO get this working
+		// Set default values
 		boolean rightHanded = true;
 		int np = 4;
 		int nt = 10;
 		int tm = 0;
 		int em = 0;
+		
+		// TODO read in command line args
 
 		// Create np forks
 		Fork[] forks = new Fork[np];
--- src/activity_2/IFork.java
+++ src/activity_2/IFork.java
@@ -1,8 +1,19 @@
 package activity_2;
 
+/**
+ * Interface for all tine-based cutlery.
+ *
+ * Supports acquisition and release.
+ */
 public interface IFork {
 
+	/**
+	 * Current thread explicitly acquires this fork
+	 */
     public void acquire();
 
+    /**
+     * Re-set acquisition status to "false"
+     */
     public void release();
 }
